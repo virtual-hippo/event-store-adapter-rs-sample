e4383f90efc2445edfb6ceb0e489499824027e1a
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-08-02 15:43:36 +0900

    [root] Added Basic Authentication

--- cdk/lib/constructs/cdn.ts
+++ cdk/lib/constructs/cdn.ts
@@ -1,3 +1,5 @@
+import * as path from "path";
+
 import {
   CfnOutput,
   aws_cloudfront as cloudfront,
@@ -14,6 +16,14 @@ export class Cdn extends Construct {
   constructor(scope: Construct, id: string, props: CdnProps) {
     super(scope, id);
 
+    // 簡易的にBasic認証を用意しておく
+    const basicAuthenticationFunction = new cloudfront.Function(this, "BasicAuthenticationFunction", {
+      code: cloudfront.FunctionCode.fromFile({
+        filePath: path.join(__dirname, "../../", "assets/cloudfront-function/basic-authentication.js"),
+      }),
+      runtime: cloudfront.FunctionRuntime.JS_2_0,
+    });
+
     const distribution = new cloudfront.Distribution(this, "ApiDistribution", {
       comment: "event-store-adapter-rs-sample",
       priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
@@ -24,6 +34,12 @@ export class Cdn extends Construct {
         cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
         originRequestPolicy: cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
         responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.SECURITY_HEADERS,
+        functionAssociations: [
+          {
+            function: basicAuthenticationFunction,
+            eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+          },
+        ],
       },
       enableLogging: false,
     });
