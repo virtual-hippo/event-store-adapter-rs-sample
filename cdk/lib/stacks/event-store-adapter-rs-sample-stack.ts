9df1bcac404452dc7c5a4dd4709059e5f4b30a92
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-08-03 23:57:37 +0900

    [cdk] Added DynamoDbTable for EventStore

--- cdk/lib/stacks/event-store-adapter-rs-sample-stack.ts
+++ cdk/lib/stacks/event-store-adapter-rs-sample-stack.ts
@@ -1,12 +1,13 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-import { AppFunction, Cdn } from "../constructs";
+import { AppFunction, Cdn, EventStore } from "../constructs";
 
-import { AppParameter } from "../parameters";
+import { AppParameter, EnvType } from "../parameters";
 
 export interface EventStoreAdapterRsSampleStackProps extends StackProps {
   readonly appParameter: AppParameter;
+  readonly envType: EnvType;
   readonly hasherFnArnParameterName: string;
 }
 
@@ -14,8 +15,20 @@ export class EventStoreAdapterRsSampleStack extends Stack {
   constructor(scope: Construct, id: string, props: EventStoreAdapterRsSampleStackProps) {
     super(scope, id, props);
 
+    const journalGsiName = `${props.envType}-journal-aid-index`;
+    const snapshotGsiName = `${props.envType}-snapshot-aid-index`;
+
+    const eventStore = new EventStore(this, "EventStore", {
+      journalGsiName: journalGsiName,
+      snapshotGsiName: snapshotGsiName,
+    });
+
     const appFunction = new AppFunction(this, "AppFunction", {
       apiParameter: props.appParameter.appFunctionParameter.apiParameter,
+      journalTable: eventStore.journalTable,
+      journalGsiName: journalGsiName,
+      snapshotTable: eventStore.snapshotTable,
+      snapshotGsiName: snapshotGsiName,
     });
 
     new Cdn(this, "Cdn", {
