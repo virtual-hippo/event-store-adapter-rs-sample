b87b0d6f53695c0a6db25e1829e9e7ee2fc47587
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-08-06 14:51:25 +0900

    [cdk] Applied formatter

--- cdk/eslint.config.mjs
+++ cdk/eslint.config.mjs
@@ -1,19 +1,14 @@
 // eslint.config.mjs
 import eslint from "@eslint/js";
-import { globalIgnores } from "eslint/config";
 import eslintCdkPlugin from "eslint-cdk-plugin";
+import { globalIgnores } from "eslint/config";
 import tsEslint from "typescript-eslint";
 
 // https://eslint-cdk-plugin.dev/ja/getting-started/
 export default [
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
-  globalIgnores([
-    "**/node_modules/**/*",
-    "**/cdk.out/**/*",
-    "**/*.js",
-    "**/*.d.ts",
-  ]),
+  globalIgnores(["**/node_modules/**/*", "**/cdk.out/**/*", "**/*.js", "**/*.d.ts"]),
   {
     files: ["lib/**/*.ts", "bin/*.ts"],
     languageOptions: {
