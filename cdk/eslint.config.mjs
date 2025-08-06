62f4f43affddae22e625a2d42e8deb0daa2f728d
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-08-06 14:17:05 +0900

    [cdk] Updated linter config

--- cdk/eslint.config.mjs
+++ cdk/eslint.config.mjs
@@ -1,10 +1,19 @@
 // eslint.config.mjs
+import eslint from "@eslint/js";
+import { globalIgnores } from "eslint/config";
 import eslintCdkPlugin from "eslint-cdk-plugin";
 import tsEslint from "typescript-eslint";
 
 // https://eslint-cdk-plugin.dev/ja/getting-started/
 export default [
+  eslint.configs.recommended,
   ...tsEslint.configs.recommended,
+  globalIgnores([
+    "**/node_modules/**/*",
+    "**/cdk.out/**/*",
+    "**/*.js",
+    "**/*.d.ts",
+  ]),
   {
     files: ["lib/**/*.ts", "bin/*.ts"],
     languageOptions: {
@@ -13,7 +22,6 @@ export default [
         project: "./tsconfig.json",
       },
     },
-    ignores: ["**/node_modules/**/*", "**/cdk.out/**/*", "**/*.js", "**/*.d.ts"],
     // ✅ Add plugins
     plugins: {
       cdk: eslintCdkPlugin,
